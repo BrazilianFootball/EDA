e4bca7cdea31b8acc13841b5df55a01931dad6ad
IgorMichels <igorxadrezbr@gmail.com>
2023-08-30 00:01:50 -0300

    added parameters

--- Modeling/models.py
+++ Modeling/models.py
@@ -8,25 +8,26 @@ from utils.independents_poisson_model import IndependentsPoissonModel
 if __name__ == '__main__':
     cleaning = True if '--c' in sys.argv else False
     today = datetime.date.today()
+    n_sims = 1_000_000
     year = today.year
 
     if cleaning: os.system('clear')
     print('Independents Poisson - Serie A')
-    IndependentsPoissonModel('Serie_A', year, 1_000_000).run_model()
+    IndependentsPoissonModel('Serie_A', year, n_sims).run_model()
     if cleaning: os.system('clear')
     print('Independents Poisson - Serie B')
-    IndependentsPoissonModel('Serie_B', year, 1_000_000).run_model()
+    IndependentsPoissonModel('Serie_B', year, n_sims).run_model()
 
     if cleaning: os.system('clear')
     print('Holgates Bivariate Poisson - Serie A')
-    HolgatesPoissonModel('Serie_A', year, 1_000_000).run_model()
+    HolgatesPoissonModel('Serie_A', year, n_sims).run_model()
     if cleaning: os.system('clear')
     print('Holgates Bivariate Poisson - Serie B')
-    HolgatesPoissonModel('Serie_B', year, 1_000_000).run_model()
+    HolgatesPoissonModel('Serie_B', year, n_sims).run_model()
 
     if cleaning: os.system('clear')
     print('Shock Models - Serie A')
-    ShockModel('Serie_A', year, 1_000_000).run_model()
+    ShockModel('Serie_A', year, n_sims).run_model()
     if cleaning: os.system('clear')
     print('Shock Models - Serie B')
-    ShockModel('Serie_B', year, 1_000_000).run_model()
+    ShockModel('Serie_B', year, n_sims).run_model()
