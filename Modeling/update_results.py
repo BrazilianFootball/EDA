6d41659cb08be4c9eb9e5ea9b4f59c8e812bbf72
IgorMichels <igorxadrezbr@gmail.com>
2023-09-05 21:29:50 -0300

    added summary

--- Modeling/update_results.py
+++ Modeling/update_results.py
@@ -1,4 +1,5 @@
 import sys
+from glob import glob
 from utils.mail_delivery import catch_results
 
 if __name__ == '__main__':
@@ -8,3 +9,22 @@ if __name__ == '__main__':
         if '-t=' in arg: tag = arg.split('=')[-1]
     
     catch_results(mail, password, tag)
+    competitions = ['Serie_A', 'Serie_B']
+    years = [*range(2013, 2024)]
+    n_games = [*range(100, 390, 10)]
+    total_ = 0
+    expected_ = 0
+    for year in years:
+        for competition in competitions:
+            total = 0
+            expected = 0
+            for games in n_games:
+                expected += 12
+                n = len(glob(f'results/images/{competition}_{year}*{games}_games*'))
+                total += n
+
+            total_ += total
+            expected_ += expected
+            if total != 0: print(f'{year}\'s {competition.replace("_", " ")} is {total / expected:.2%} completed')
+
+    print(f'\nAll is {total_ / expected_:.2%} completed')
